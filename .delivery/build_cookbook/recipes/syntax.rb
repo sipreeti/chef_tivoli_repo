a7c8235f554bcbc7aa02b15646a38a31d5260f44
Preeti Singh <preeti.singh3008@gmail.com>
2021-07-25 17:19:53 -0700

    initial commit

---
+++ .delivery/build_cookbook/recipes/syntax.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: build_cookbook
+# Recipe:: syntax
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+include_recipe 'delivery-truck::syntax'
---
+++ recipes/r_tivoli.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: tivoli_cookbook
+# Recipe:: r_tivoli
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
